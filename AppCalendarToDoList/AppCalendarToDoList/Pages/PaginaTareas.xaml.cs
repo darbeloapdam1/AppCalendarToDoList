215259f7b90d28f737b8edec74aca36151c46290
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-02-06 22:51:17 +0100

    implementar parte de la navegacion entre paginas

--- AppCalendarToDoList/AppCalendarToDoList/Pages/PaginaTareas.xaml.cs
+++ AppCalendarToDoList/AppCalendarToDoList/Pages/PaginaTareas.xaml.cs
@@ -11,12 +11,35 @@ using AppCalendarToDoList.Pages;
 namespace AppCalendarToDoList.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PaginaTareas : ContentPage
+    public partial class PaginaTareas : FlyoutPage
     {
         public PaginaTareas()
         {
             InitializeComponent();
             
+            actualizarTareas();
+        }
+
+        private void actualizarTareas()
+        {
+            foreach(Tarea tarea in getTareas())
+            {
+                grdTareas.RowDefinitions.Add(new RowDefinition { Height = new GridLength(180) });
+                int numFila = grdTareas.RowDefinitions.Count;
+                //grdTareas.Children.Add(tarea,0,numFila);
+            }
+        }
+
+        private List<Tarea> getTareas()
+        {
+            List<Tarea> listaTareas = new List<Tarea>();
+
+            return listaTareas;
+        }
+        private void btnNuevaTarea_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PaginaNuevaTarea());
+            IsPresented = false;
         }
     }
 }
