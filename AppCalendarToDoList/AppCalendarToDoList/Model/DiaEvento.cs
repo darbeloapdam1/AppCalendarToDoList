57a559f32e7d3dddc2ff9488b7915e6d6470c969
darbeloapdam1 <darbeloapdam1@gmail.com>
2022-02-08 00:34:25 +0100

    añadir eventos a la paginaEventos

---
+++ AppCalendarToDoList/AppCalendarToDoList/Model/DiaEvento.cs
@@ -0,0 +1,95 @@
+﻿using AppCalendarToDoList.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+
+namespace AppCalendarToDoList.Model
+{
+    public class DiaEvento
+    {
+        DateTime fecha;
+        List<EventoViewModel> eventos;
+        List<TareaViewModel> tareas;
+        Grid grid;
+        string diaSemana, dia, mes;
+        public DiaEvento(DateTime fecha)
+        {
+            this.fecha = fecha;
+            this.dia = "" + fecha.Day;
+            this.diaSemana = fecha.DayOfWeek.ToString();
+            Grid = crearGridDia();
+        }
+
+        public Grid Grid
+        {
+            get { return grid; }
+            set
+            {
+                this.grid = value;
+            }
+        }
+
+        private Grid crearGridDia()
+        {
+            Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLength(30) },
+                    new RowDefinition { Height = new GridLength(80) }
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(160) }
+                }
+            };
+
+            grid.Children.Add(new Frame
+            {
+                CornerRadius = 10,
+                BackgroundColor = Color.Transparent,
+                BorderColor = Color.White,
+            }, 0, 0);
+
+            grid.Children.Add(new Label
+            {
+                Text = diaSemana,
+                FontSize = 24,
+                Margin = new Thickness(10, 0, 0, 0)
+            }, 0, 0);
+
+            grid.Children.Add(new Label
+            {
+                Text = dia,
+                FontSize = 32,
+                TextColor = Color.White,
+                Margin = new Thickness(10, 0, 100, 0)
+            }, 1, 0);
+
+            grid.Children.Add(new Line
+            {
+                BackgroundColor = Color.Red,
+                Margin = new Thickness(35, 10, 124, 10)
+            }, 0, 1);
+
+            grid.Children.Add(new Label
+            {
+                Text = "Ningún evento",
+                Margin = new Thickness(60, 15, 0, 0)
+            }, 0, 1);
+
+            grid.Children.Add(new Label
+            {
+                Text = "Ninguna tarea",
+                Margin = new Thickness(60, 45, 0, 0)
+            }, 0, 1);
+
+            return grid;
+        }
+
+        
+
+    }
+}
--- AppCalendarToDoList/AppCalendarToDoList/Pages/PaginaEventos.xaml.cs
+++ AppCalendarToDoList/AppCalendarToDoList/Pages/PaginaEventos.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using AppCalendarToDoList.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 using Xamarin.Forms.Xaml;
 
 namespace AppCalendarToDoList.Pages
@@ -15,13 +17,123 @@ namespace AppCalendarToDoList.Pages
         public PaginaEventos()
         {
             InitializeComponent();
-            
+            pckAnio.ItemsSource = getAniosPck();
+            pckMes.SelectedIndex = 0;
+            pckAnio.SelectedIndex = 0;
+            actualizarPagina();
         }
 
         private void btnNuevoEvento_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new PaginaNuevoEvento());
         }
-        
+
+        public void actualizarPagina()
+        {
+            int mesNum = getMesNum(pckMes.Items[pckMes.SelectedIndex]);
+            int anio = int.Parse(pckAnio.Items[pckAnio.SelectedIndex]);
+            DateTime fecha = new DateTime(anio, mesNum,1);
+            List<DiaEvento> diasMes = getDiasMes(fecha);
+            addDiasGrid(diasMes);
+        }
+
+        private List<DiaEvento> getDiasMes(DateTime fecha)
+        {
+            DateTime fechaDia = fecha;
+            List<DiaEvento> lista = new List<DiaEvento>();
+            for(int i = 0; i < DateTime.DaysInMonth(fecha.Year, fecha.Month); i++)
+            {
+                lista.Add(new DiaEvento(fechaDia));
+                fechaDia = fechaDia.AddDays(1);
+            }
+            return lista;
+        }
+
+        public static int getMesNum(string mes)
+        {
+            if (mes.Equals("Enero"))
+            {
+                return 1;
+            }else if (mes.Equals("Febrero"))
+            {
+                return 2;
+            }else if (mes.Equals("Marzo"))
+            {
+                return 3;
+            }else if (mes.Equals("Abril"))
+            {
+                return 4;
+            }else if (mes.Equals("Mayo"))
+            {
+                return 5;
+            }else if (mes.Equals("Junio"))
+            {
+                return 6;
+            }else if (mes.Equals("Julio"))
+            {
+                return 7;
+            }else if (mes.Equals("Agosto"))
+            {
+                return 8;
+            }else if (mes.Equals("Septiembre"))
+            {
+                return 9;
+            }else if (mes.Equals("Octubre"))
+            {
+                return 10;
+            }else if (mes.Equals("Noviembre"))
+            {
+                return 11;
+            }else if (mes.Equals("Diciembre"))
+            {
+                return 10;
+            }
+            return 0;
+        }
+
+        private List<int> getAniosPck()
+        {
+            List<int> lista = new List<int>();
+
+            for (int i = 2000; i < 2025; i++)
+            {
+                lista.Add(i);
+            }
+
+            return lista;
+        }
+
+        private void addDiasGrid(List<DiaEvento> dias)
+        {
+            grdEventos.RowDefinitions.Add(new RowDefinition { Height = new GridLength(40) });
+            for (int i = 0; i < dias.Count; i++)
+            {
+                int columna = 0;
+                if(i % 2 == 0 && i != 0)
+                {
+                    columna = 0;
+                    grdEventos.RowDefinitions.Add(new RowDefinition { Height = new GridLength(110) });
+                }
+                else
+                {
+                    if(i != 0)
+                    {
+                        columna = 2;
+                    }                   
+                }
+                grdEventos.Children.Add(dias[i].Grid, columna, grdEventos.RowDefinitions.Count);
+                
+            }
+        }
+
+        private void pckMes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            actualizarPagina();
+        }
+
+        private void pckAnio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            actualizarPagina();
+        }
     }
 }
