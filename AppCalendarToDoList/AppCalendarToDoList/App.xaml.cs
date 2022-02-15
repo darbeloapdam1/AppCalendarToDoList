e20759fb5fee7712c078958dd0122182d5e364aa
eoloritzadam1 <eoloritzadam1@gmail.com>
2022-02-15 09:34:31 +0100

    Conexion con SQLite

--- AppCalendarToDoList/AppCalendarToDoList/App.xaml.cs
+++ AppCalendarToDoList/AppCalendarToDoList/App.xaml.cs
@@ -2,16 +2,32 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppCalendarToDoList.Data;
+using System.IO;
 
 namespace AppCalendarToDoList
 {
     public partial class App : Application
     {
+        static SQLiteHelper db;
         public App()
         {
             InitializeComponent();
 
             MainPage = new PaginaMenu();
+        }
+        public static SQLiteHelper SQLiteDB
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine
+                        (Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "Eventos.db3"));
+                }
+                return db;
+            }
         }
 
         protected override void OnStart()
